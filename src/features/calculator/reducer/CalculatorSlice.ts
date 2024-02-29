fa28701711a129b448100d9b1b9894fabf608782
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-03-01 00:39:29 +0300

    fix(reducer) logic for input '.', refactor logic for display value and clear value

--- src/features/calculator/reducer/CalculatorSlice.ts
+++ src/features/calculator/reducer/CalculatorSlice.ts
@@ -2,10 +2,12 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface initialStateType {
     displayValue: string
+    currentValue: string
 }
 
 const initialState: initialStateType = {
   displayValue: '0',
+  currentValue: '0',
 };
 
 const calculatorSlice = createSlice({
@@ -13,10 +15,29 @@ const calculatorSlice = createSlice({
   initialState,
   reducers: {
     inputDigit: (state, action: PayloadAction<string>) => {
-      state.displayValue = action.payload;
+      if (state.currentValue === '0') {
+        state.currentValue = action.payload.toString();
+      } else {
+        state.currentValue += action.payload.toString();
+      }
+      state.displayValue = state.currentValue;
+    },
+    inputDecimal: (state) => {
+      if (!state.currentValue.includes('.')) {
+        state.currentValue += '.';
+        state.displayValue = state.currentValue;
+      }
+    },
+    clearCalculator: (state) => {
+      state.displayValue = '0';
+      state.currentValue = '0';
     },
   },
 });
-export const {inputDigit} = calculatorSlice.actions;
+export const {
+  inputDigit,
+  inputDecimal,
+  clearCalculator,
+} = calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
