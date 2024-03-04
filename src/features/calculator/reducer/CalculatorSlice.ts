185ee6317d302a301b8e9f763ac17328409026b1
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-03-04 17:17:11 +0300

    fix(slice) added new function for input operator and equal

--- src/features/calculator/reducer/CalculatorSlice.ts
+++ src/features/calculator/reducer/CalculatorSlice.ts
@@ -1,13 +1,36 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
+export type Nullable<T> = T | null
+
 interface initialStateType {
     displayValue: string
     currentValue: string
+    operator: Nullable<string>
+    previousValue: Nullable<string>
 }
 
 const initialState: initialStateType = {
   displayValue: '0',
   currentValue: '0',
+  operator: null,
+  previousValue: null,
+};
+
+const evaluate = (a: string, b: string, operator: string): string => {
+  const numA = parseFloat(a);
+  const numB = parseFloat(b);
+  switch (operator) {
+  case '+':
+    return (numA + numB).toString();
+  case '-':
+    return (numA - numB).toString();
+  case 'x':
+    return (numA * numB).toString();
+  case '/':
+    return (numA / numB).toString();
+  default:
+    return b;
+  }
 };
 
 const calculatorSlice = createSlice({
@@ -28,6 +51,33 @@ const calculatorSlice = createSlice({
         state.displayValue = state.currentValue;
       }
     },
+    inputOperator: (state, action: PayloadAction<{ operator: string }>) => {
+      const { operator } = action.payload;
+      if (state.operator === null) {
+        state.previousValue = state.currentValue;
+      } else {
+        state.previousValue = evaluate(
+            state.previousValue!,
+            state.currentValue!,
+            state.operator,
+        );
+      }
+      state.currentValue = '0';
+      state.displayValue = state.previousValue!;
+      state.operator = operator;
+    },
+    calculateResult: (state) => {
+      if (state.operator !== null) {
+        state.displayValue = evaluate(
+            state.previousValue!,
+            state.currentValue,
+            state.operator,
+        );
+        state.currentValue = state.displayValue;
+        state.previousValue = null;
+        state.operator = null;
+      }
+    },
     clearCalculator: (state) => {
       state.displayValue = '0';
       state.currentValue = '0';
@@ -38,6 +88,8 @@ export const {
   inputDigit,
   inputDecimal,
   clearCalculator,
+  inputOperator,
+  calculateResult,
 } = calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
