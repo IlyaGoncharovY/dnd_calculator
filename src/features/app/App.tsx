1a7738d35dc5c72e3763468804fec126b8be057b
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-03-11 17:13:46 +0300

    fix(app) render Dashboard.tsx

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,13 +1,10 @@
-import {Constructor, DropContainer} from '../dnd';
+import {DashBoardPage} from '../dnd';
 
 function App() {
 
   return (
-    <div style={{display: 'flex', width: '700px', margin: 'auto', gap: '40px'}}>
-      <div style={{border: '1px solid black', flex: 1}}>
-        <Constructor/>
-      </div>
-      <DropContainer/>
+    <div style={{display: 'flex',width: '600px', margin: 'auto', gap: '40px'}}>
+      <DashBoardPage/>
     </div>
   );
 }
